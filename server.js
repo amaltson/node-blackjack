55787f3f9c43048545eb6f0344c643cbfffc55d7
Arthur Kalmenson <arthur.kalm@gmail.com>
2011-05-12 07:55:27 -0400

    Trying out Jade. Issue #21.

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require.paths.unshift('./node_modules')
 
 var express = require('express');
 var app = express.createServer();
+var jade = require('jade');
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  jade.renderFile('views/index.jade', function(err, html) {
+   /* res.send(html);*/
+    res.send('hello, world');
+  });
 });
 
 app.listen(9000);
