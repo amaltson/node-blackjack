2ccf146e4cc35ed190de56ded17735c51eb86c83
Ahmed Javed <ahmed.javed@gmail.com>
2011-05-19 00:33:33 -0400

    Initial setup of client that logs sockets messages

--- public/js/client.js
+++ public/js/client.js
@@ -1,26 +1,37 @@
-/* Author: 
+/* Author: Ahmed Javed 
+ */
+var blackjackClient = {
+socket : "",
+maximumNumberOfLogs : 3
+};
 
-*/
+blackjackClient.connectToServer = function() {
+  var aBlackjackClientInstance = this;
+  aBlackjackClientInstance.socket = new io.Socket('localhost');
+  aBlackjackClientInstance.socket.connect();
+  aBlackjackClientInstance.socket.on('connect', function() {
+    blackjackClient.logMessage("Connected to server");
+  });
 
+  this.socket.on('message', function(msg) {
+    aBlackjackClientInstance.logMessage.apply(aBlackjackClientInstance, [ "Message recieved:" + msg ]);
+  });
 
+  this.socket.on('close', function() {
+    aBlackjackClientInstance.logMessage("Connection closed");
+  });
+  this.socket.on('disconnect', function() {
+    aBlackjackClientInstance.logMessage("Connection disconnected");
+  });
+};
 
+blackjackClient.logMessage = function(message) {
+  if ($('#logList li').length === this.maximumNumberOfLogs) {
+    $('#logList li:first').remove();
+  }
+  $('#logList').append("<li>" + message + "</li>");
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+  blackjackClient.connectToServer();
+});
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer();
 
 app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.listen(9000);
@@ -12,6 +12,6 @@ app.listen(9000);
 var io = require('socket.io');
 var socket = io.listen(app);
 socket.on('connection', function(client) {
-
-  console.log('connected with socket.io');
+	console.log('connected with socket.io');
+	client.send('This is your server. Thanks for connecting to me.');
 });
