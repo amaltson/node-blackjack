8fba384cfb1f703150c154ed957943189fa719fc
Jeff Chan <jphchan@gmail.com>
2011-05-19 00:32:08 -0400

    Began some work integrating jasmine-node into the project
    Change-Id: I4de6e5a3e19fd7ec03665373fd5e5ae60578f3e2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require.paths.unshift('./node_modules')
+require.paths.unshift('./node_modules');
 
 var express = require('express');
 var app = express.createServer();
 
 app.configure(function() {
-  app.use(express.static(__dirname + '/views'));
+	app.use(express.static(__dirname + '/views'));
 });
 
 app.listen(9000);
@@ -13,5 +13,5 @@ var io = require('socket.io');
 var socket = io.listen(app);
 socket.on('connection', function(client) {
 
-  console.log('connected with socket.io');
+	console.log('connected with socket.io');
 });
