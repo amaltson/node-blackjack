f7e881bfa9dd4fce261c1dcb27b971a9c587c503
Arthur Kalmenson <arthur.kalm@gmail.com>
2011-05-26 22:54:53 -0400

    Implemented simple turn tracking. #35.
    - just keeps an array of userIds around and cycles through it.
    - tests added.

--- scripts/gameState.js
+++ scripts/gameState.js
@@ -1,6 +1,8 @@
 var Blackjack = module.exports = function Blackjack() {
 
   this.players = {};
+  this.table = [];
+  this.current = 0;
 }
 
 Blackjack.prototype = {
@@ -9,6 +11,7 @@ Blackjack.prototype = {
     // assume getting a vararg
     for (var i = 0; i < arguments.length; i++) {
       this.players[arguments[i].userId] = arguments[i];
+      this.table.push(arguments[i].userId);
     }
   },
 
@@ -22,5 +25,16 @@ Blackjack.prototype = {
       result.push(this.players[userId]);
     }
     return result;
-  }
+  },
+
+  nextTurn : function() {
+    var result = this.table[this.current];
+    if (this.current < this.table.length - 1) {
+      this.current += 1;
+    } else {
+      this.current = 0;
+    }
+
+    return result;
+  },
 }
--- tests/gameStateTest.js
+++ tests/gameStateTest.js
@@ -17,12 +17,23 @@ module.exports = testCase({
 
       blackjack.addPlayer(player);
       test.equals(blackjack.getPlayer(player.userId), player, 'players equal');
+      test.equals(blackjack.table.length, 1, 'player added to table');
       test.done();
     },
 
     'get all players' : function(test) {
       blackjack.addPlayer({userId: 'hello'}, {userId: 'hi'});
       test.equals(blackjack.getAllPlayers().length, 2, 'Got back two players');
+      test.equals(blackjack.table.length, 2, 'players added to table');
+      test.done();
+    },
+
+    'player turns' : function(test) {
+      blackjack.addPlayer({userId: 'hello'}, {userId: 'hi'});
+      test.equals(blackjack.nextTurn(), 'hello', 'first in, first player');
+      test.equals(blackjack.nextTurn(), 'hi', 'second player');
+      test.equals(blackjack.nextTurn(), 'hello', 'back to first');
+      test.equals(blackjack.nextTurn(), 'hi', 'second player again');
       test.done();
     },
 });
