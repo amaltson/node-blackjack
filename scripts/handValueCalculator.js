8e77b162a2a25e43b3c311bb44df98ddf34fad4f
Jeff Chan <jphchan@gmail.com>
2011-06-03 16:17:00 -0400

    Fixing up the solution for lab1
    Change-Id: I45b7d94f28757a4d2272be87bb8cbe3e298c3b83

--- scripts/handValueCalculator.js
+++ scripts/handValueCalculator.js
@@ -13,46 +13,5 @@ module.exports = function HandValueCalculator() {
 
 	// the hand value calculator
 	this.calculateHandValue = function(aHand) {
-		var totalValue = 0;
-		var numberOfAces = 0;
-
-		// protect against empty argument list or empty collection
-		if (this.isUndefined(aHand) || aHand.length === 0) {
-			return totalValue;
-		}
-
-		// Calculate the non-Ace hand value, and count up the Aces. They're
-		// trickier
-		// so we'll do them later.
-		for ( var i = 0; i < aHand.length; i++) {
-			var aCard = aHand[i];
-			var cardValue = parseInt(aCard.type);
-			if (isNaN(cardValue)) {
-				switch (aCard.type) {
-				case 'A':
-					cardValue = 0;
-					numberOfAces++;
-					break;
-				case 'J':
-				case 'Q':
-				case 'K':
-					cardValue = 10;
-					break;
-				default:
-					throw 'calculateHandValue cannot convert unrecognized card type';
-				}
-			}
-			totalValue += cardValue;
-		}
-
-		// Now we handle the case of the Aces, they're trickier because they can
-		// be 1 or 11.
-		if (totalValue < this.BLACKJACK) {
-			for ( var j = 0; j < numberOfAces; j++) {
-				cardValue = (totalValue > 10) ? 1 : 11;
-				totalValue += cardValue;
-			}
-		}
-		return (totalValue > this.BLACKJACK) ? this.BUSTED_VALUE : totalValue;
 	};
 };
