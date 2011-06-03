6cb4b28be58c818cd59f2e7ce3cc40b58230c08f
Jeff Chan <jphchan@gmail.com>
2011-06-03 16:19:13 -0400

    added in lab 1 tests.
    Change-Id: Ice8f1bbe903338464bffa21147afebae9bd41a5a

--- tests/lab1TestSuite.js
+++ tests/lab1TestSuite.js
@@ -1,20 +1,45 @@
 fibTestSuite = {
-	'Sample Test Code' : function(test) {
+	'fib with 0' : function(test) {
 		setTimeout(function() {
 			test.expect(1); // Expecting to test only 1 assertion
-			var actual = 0; // The tested Result
+			var actual = fib(0); // The tested Result
 			var expected = 0; // The expected Result
 			test.strictEqual(actual, expected,
-					"Some message indicating why it failed");
+					"Fib(0) should have been 0 but was " + actual);
 			test.done(); // To stop the test and indicate that it's done
 		}, 10); // How long to wait before quitting the test
 	},
 
-	'Your Tests Start Here' : function(test) {
+	'fib with 1' : function(test) {
+		setTimeout(function() {
+			test.expect(1); // Expecting to test only 1 assertion
+			var actual = fib(1); // The tested Result
+			var expected = 1; // The expected Result
+			test.strictEqual(actual, expected,
+					"Fib(1) should have been 1 but was " + actual);
+			test.done(); // To stop the test and indicate that it's done
+		}, 10); // How long to wait before quitting the test
+	},
+
+	'fib with 2' : function(test) {
+		setTimeout(function() {
+			test.expect(1); // Expecting to test only 1 assertion
+			var actual = fib(2); // The tested Result
+			var expected = 1; // The expected Result
+			test.strictEqual(actual, expected,
+					"Fib(2) should have been 1 but was " + actual);
+			test.done(); // To stop the test and indicate that it's done
+		}, 10); // How long to wait before quitting the test
+	},
+
+	'fib with 10' : function(test) {
 		setTimeout(function() {
 			test.expect(1);
-			test.ok(false, "This test should fail");
-			test.done();
+			var actual = fib(10); // The tested Result
+			var expected = 55; // The expected Result
+			test.strictEqual(actual, expected,
+					"Fib(10) should have been 55 but was " + actual);
+			test.done(); // To stop the test and indicate that it's done
 		}, 10);
 	}
 };
